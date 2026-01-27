798caa3f3ebe265d4974218d0e169bd38e0d6bcd
Farel Anugrah Al Fauzan <farel0205@Farels-MacBook-Air.local>
2026-01-27 14:41:54 +0700

    commit 3

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => [
     'http://localhost:3000',
     'http://10.45.196.166:3000',
-    'https://frontendgrowtechtesting1-production.up.railway.app/'
+    'https://frontendgrowtechtesting1-production.up.railway.app'
     ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
