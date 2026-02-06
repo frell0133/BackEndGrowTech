8b80011a5c100b848e6deb82ab0c08adfa5751df
Farel Anugrah Al Fauzan <farel0205@Farels-MacBook-Air.local>
2026-02-06 20:57:27 +0700

    commit gak tahu lagi lah9

---
+++ resources/views/emails/reset-password.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Password</title>
+</head>
+<body>
+    <p>Klik link berikut untuk reset password:</p>
+
+    <p>
+        <a href="{{ $resetUrl }}">
+            Reset Password
+        </a>
+    </p>
+
+    <p>Jika bukan kamu, abaikan email ini.</p>
+</body>
+</html>
