61307670915ac4a8cd1a81af12fb34023ada30a8
Farel Anugrah Al Fauzan <farel0205@Farels-MacBook-Air.local>
2026-02-25 20:42:57 +0700

    Cart to order k34

--- config/services.php
+++ config/services.php
@@ -6,12 +6,13 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
     */
-    'brevo' => [
-        'key' => env('BREVO_API_KEY'),
-        'sender_email' => env('BREVO_SENDER_EMAIL'),
-        'sender_name' => env('BREVO_SENDER_NAME', 'GrowTech Central'),
-    ],
 
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
@@ -33,7 +34,7 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
-
+    
     'supabase' => [
         'url' => env('SUPABASE_URL'),
         'service_role_key' => env('SUPABASE_SERVICE_ROLE_KEY'),
@@ -43,8 +44,9 @@ return [
         'sign_expires' => (int) env('SUPABASE_SIGN_EXPIRES', 60),
         'download_expires' => (int) env('SUPABASE_DOWNLOAD_EXPIRES', 600),
         'public_banners_base' => env('SUPABASE_PUBLIC_BANNERS_BASE'),
+        'public_banners_base' => env('SUPABASE_PUBLIC_BANNERS_BASE'),
     ],
-
+    
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
@@ -61,6 +63,13 @@ return [
         'server_key' => env('MIDTRANS_SERVER_KEY'),
         'snap_url'   => env('MIDTRANS_SNAP_URL', 'https://app.sandbox.midtrans.com/snap/v1/transactions'),
         'simulate'   => env('MIDTRANS_SIMULATE', false),
-    ],
+
+    'brevo' => [
+        'key' => env('BREVO_KEY'),
+        'sender_email' => env('BREVO_SENDER_EMAIL'),
+        'sender_name' => env('BREVO_SENDER_NAME', 'GrowTech Central'),
+],
+],
+
 
 ];
