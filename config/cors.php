4e2d7f747678bfd6f120b9c2cd718fd1c469ba6d
Farel Anugrah Al Fauzan <farel0205@Farels-MacBook-Air.local>
2026-03-28 17:01:52 +0700

    Optimalisasi API K8 revisi5

--- config/cors.php
+++ config/cors.php
@@ -10,7 +10,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://frontendgrowtechtesting1-production-dfb9.up.railway.app',
+        // 'https://frontendgrowtechtesting1-production-dfb9.up.railway.app',
         'https://frontendgrowtechtesting1-production-6d21.up.railway.app',
         'http://localhost:3000',
     ],
